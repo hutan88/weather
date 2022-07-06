dad2a915cfa3f538e191f5c56e3f36d8ade3f76c
Hutan Alinaghi <hutan.alinaghi88@gmail.com>
2022-07-06 12:26:57 +0330

    Initial files and images

---
+++ app.js
@@ -0,0 +1,2 @@
+const urlIP="http://ip-api.com/json/?fields=country,city,lat,lon,timezone";
+const urlWeather=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=9bd1e4553c53b771c19ecb669bdaf92b`;   
